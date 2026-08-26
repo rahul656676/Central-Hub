49990f5fa9288014f1029476b5808b9951c32c5c
animesh4302-blip <dubeyanimesh31@gmail.com>
2026-08-27 02:05:31 +0530

    feat(wave-2): add loitering detection plugin and local sqlite offline alert queue

--- camera_agent/registry.py
+++ camera_agent/registry.py
@@ -1,7 +1,8 @@
 from usecases.ppe_check import PPECheckPlugin
+from usecases.loitering_detection import LoiteringDetectionPlugin
 
 # Maps use case string identifiers from the database to their implementation classes
 USECASE_REGISTRY = {
     "ppe_monitoring": PPECheckPlugin,
-    # Additional use cases will be registered here (anpr, loitering, etc.)
+    "loitering_detection": LoiteringDetectionPlugin,
 }
